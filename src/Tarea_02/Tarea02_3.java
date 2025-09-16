3ac70169d06ea4e6bee60c72f91bb589479bfdb5
oliver <omiguezalonso@gmail.com>
2025-09-16 10:05:01 +0200

    Tarea 02

--- src/Tarea_02/Tarea02_3.java
+++ src/Tarea_02/Tarea02_3.java
@@ -8,7 +8,15 @@ import java.util.Properties;
 public class Tarea02_3 {
     public static void main(String[] args) {
         Properties st = System.getProperties();
-        System.out.println(st);
+        //System.out.println(st);
 
+        //Con el toString dividimos todos los procesos con comas
+        String propiedades = st.toString();
+        //Almacenamos cada proceso separado por una coma
+        String[]prop = propiedades.split(",");
+        //Recorremos el array que tiene almacenado todos los procesos y los mostramos ordenados
+        for(String p : prop){
+            System.out.println(p);
+        }
     }
 }
