b0040353b06dd00d02090cb6984d4232caf6bd78
XogosAquiDEV <olimial03@gmail.com>
2025-09-16 17:54:14 +0200

    Fin eje 3 psp

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
 Crea un programa en Java que permita al usuario, desde la línea de comandos, abrir un archivo existente o crear uno nuevo
@@ -8,11 +9,17 @@ Si el archivo no existe, el editor deberá permitir su creación.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+        //crea un objeto escáner para preguntar por pantalla
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nombre del archivo a abrir o crear: ");
+        String argumento1 = sc.nextLine();//nombre del archivo por abrir(si existe) si no existe lo crea
+
         //comando que abre el bloc de notas de windows
-        String comando = "notepad";
-        //Crea un objeto tipo ProcessBuilder con el valor del comando anterior
-        ProcessBuilder pb = new ProcessBuilder(comando);
-        //A través de un objeto Process activamos el comando del ProcessBuilder
+        String comando = "notepad.exe";
+        //Crea un objeto tipo ProcessBuilder con el valor del comando anterior y el nombre del archivo a abrir o crear
+        ProcessBuilder pb = new ProcessBuilder(comando, argumento1);
+        //A través de un objeto Process activamos el comando del ProcessBuilder que abre el archivo o lo crea
         Process p  = pb.start();
+
     }
 }
