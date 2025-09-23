054f128bea8f1180f4f6e622cbb980db6215d527
oliver <omiguezalonso@danielcastelao.org>
2025-09-23 09:15:31 +0200

    Tarea 5

---
+++ Tarea 05/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 1. Pide por consola al usuario un comando y sus parámetros a ejecutar (si fuese necesario) (Por ejemplo, ls, gnome-text-editor, open...)
+ * 2. Lanza el proceso y obtén el código de finalización del mismo.
+ * 3. Muestra el nombre del programa y el código de finalización del mismo.
+ *
+ * El programa finaliza cuando un usuario introduce “salir” y devolverá un código de salida = 0.
+ * Prueba a introducir comandos y/o parámetros inexistentes y observa el código de finalización.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
