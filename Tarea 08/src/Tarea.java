5856d24316b8dd8725a8c943056055a11ae25f08
oliver <omiguezalonso@danielcastelao.org>
2025-09-30 09:29:56 +0200

    Ejercicio 08

--- Tarea 08/src/Tarea.java
+++ Tarea 08/src/Tarea.java
@@ -1,11 +1,9 @@
-import javax.swing.text.StyledEditorKit;
 
 /**
  * Haz un programa que cree cuatro hilos y los ejecute de manera simultánea,
  * pero que no espere por la finalización de los mismos.
  * Cada hilo representará a un profesor cuyo "nivel de cabreo"
  * aumenta en 1 por cada tarea que descubre que ha sido copiada y pegada de una IA.
- *
  * Al crear cada hilo de profesor, se le debe pasar su nombre y su
  * "límite de paciencia" (el número máximo de trabajos a revisar).
  * La ejecución debe resultar en que las quejas de los cuatro hilos se mezclen en la consola.
@@ -13,7 +11,7 @@ import javax.swing.text.StyledEditorKit;
 
 public class Tarea extends Thread{
     //Constructor
-    private int paciencia = 0;
+    private final int paciencia;
     public Tarea(String str, int paciencia){
         super(str);
         this.paciencia = paciencia;
