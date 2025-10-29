95da7d79bbbfa3be52ce40e0fb48ebf12a8637db
oliver <omiguezalonso@danielcastelao.org>
2025-10-29 09:57:27 +0100

    Eje 23: Empezado

---
+++ Tarea 23/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Escribe un programa en Java que incluya una clase llamada SuperMercado, encargada de simular el funcionamiento de un supermercado con N cajas.
+ * En este escenario, M clientes pasarán un tiempo aleatorio "haciendo la compra" y, posteriormente, elegirán de manera aleatoria en qué caja ponerse,
+ * uniéndose a la cola correspondiente.
+ * Cada cliente será atendido en su turno, efectuará el pago correspondiente, y el importe pagado se añadirá a la variable Resultados del supermercado.
+ * El programa debe crear un thread independiente para cada cliente, y los valores de M y N deben proporcionarse como argumentos al ejecutarlo.
+ * Para simplificar la implementación, el importe pagado por cada cliente puede generarse de forma aleatoria en el momento de realizar su pago.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
