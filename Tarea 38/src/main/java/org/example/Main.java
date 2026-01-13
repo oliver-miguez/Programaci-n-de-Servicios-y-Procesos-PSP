d4fa73a54401918c34cbb3182d4b9b19e9182414
oliver <omiguezalonso@danielcastelao.org>
2026-01-13 09:30:39 +0100

    Tarea 38 refactorizada

--- Tarea 38/src/main/java/org/example/Main.java
+++ Tarea 38/src/main/java/org/example/Main.java
@@ -10,25 +10,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String contrasena_registro = crearContrasena(sc);
+        System.out.println("Crea una contraseña:");
+        String contrasena = sc.next();
+
+        String contrasena_registro = generadorHash(contrasena);
 
         System.out.println("Usuario Registrado. Inicie sesión: ");
         String contrasena_inicio_sesion = sc.next();
-        String contrasena_inicio_hash = calculo_hax_inicio_sesion(contrasena_inicio_sesion);
+
+        String contrasena_inicio_hash = generadorHash(contrasena_inicio_sesion);
 
         comparacionHash(contrasena_registro,contrasena_inicio_hash);
 
-
-
     }
 
-    public static String crearContrasena(Scanner sc){
+    public static String generadorHash(String dato){
 
-        System.out.println("Crea una contraseña:");
-        String contrasena = sc.next();
         try{
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(contrasena.getBytes());
+            md.update(dato.getBytes());
             byte[] resumen = md.digest();
 
             String hex = HexFormat.of().formatHex(resumen);
@@ -40,20 +40,7 @@ public class Main {
         }
     }
 
-    public static String calculo_hax_inicio_sesion(String contrasena_recibida){
-        try{
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(contrasena_recibida.getBytes());
-            byte[] resumen = md.digest();
 
-            String hex = HexFormat.of().formatHex(resumen);
-            System.out.println(hex);
-            return hex;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 
     public static void comparacionHash(String contrasena_creada, String contrasena_inicio_sesion){
         if (Objects.equals(contrasena_creada, contrasena_inicio_sesion)){
